8826993ff0320874bd9cf6860d52692009ea99ea
Garrick Kilpack <garrickkilpack@hotmail.com>
2021-04-05 16:05:15 -0600

    Initial commit
    Initial commit

---
+++ Assets/Scripts/Data/FloatData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FloatData")]
+public class FloatData : ScriptableObject
+{
+    public float value;
+}

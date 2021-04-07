59ea1cb22aad12bbda20c112a8a6c4efdb1b422f
Garrick Kilpack <garrickkilpack@hotmail.com>
2021-04-06 21:06:07 -0600

    Added FPS UI
    Physics FPS can be manipulated, fps can be seen in the corner.

--- Assets/Scripts/Engine/World.cs
+++ Assets/Scripts/Engine/World.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class World : MonoBehaviour
 {
     public BoolData simulate;
     public FloatData gravity;
+    public FloatData fixedFPS;
+    public TMP_Text fpsText = null;
     public bool useExplicit = false;
+
+    float timeAccumulator;
+    float fpsUpdateCounter;
 
     static World instance;
     public static World Instance { get { return instance; } }
 
+    public float fixedDeltaTime { get { return (1.0f / fixedFPS.value); } }
     public Vector2 Gravity { get { return new Vector2(0, gravity.value); } }
     public List<Body> bodies { get; set; } = new List<Body>();
 
@@ -21,19 +28,35 @@ public class World : MonoBehaviour
 
     void Update()
     {
+        fpsUpdateCounter++;
+
         if (!simulate.value) return;
 
         float dt = Time.deltaTime;
+        timeAccumulator += dt;
 
-        bodies.ForEach(body => body.Step(dt));
-        if (useExplicit)
+        if (fpsUpdateCounter >= 250)
         {
-            bodies.ForEach(body => Integrator.ExplicitEuler(body, dt));
+            float fps = (1.0f / dt);
+            fpsText.text = $"FPS: {fps.ToString("F1")}";
+            fpsUpdateCounter = 0;
         }
-        else
+
+        while (timeAccumulator > fixedDeltaTime)
         {
-            bodies.ForEach(body => Integrator.SemiImplicitEuler(body, dt));
+            bodies.ForEach(body => body.Step(fixedDeltaTime));
+            if (useExplicit)
+            {
+                bodies.ForEach(body => Integrator.ExplicitEuler(body, fixedDeltaTime));
+            }
+            else
+            {
+                bodies.ForEach(body => Integrator.SemiImplicitEuler(body, fixedDeltaTime));
+            }
+
+            timeAccumulator -= fixedDeltaTime;
         }
+        
 
         bodies.ForEach(body => body.force = Vector2.zero);
         bodies.ForEach(body => body.acceleration = Vector2.zero);

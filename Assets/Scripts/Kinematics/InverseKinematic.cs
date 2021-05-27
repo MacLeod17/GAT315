168cbf496bd771277cd044c5a18664efebd7c6ca
Garrick Kilpack <garrickkilpack@hotmail.com>
2021-05-26 21:13:06 -0600

    Started working with shaders

--- Assets/Scripts/Kinematics/InverseKinematic.cs
+++ Assets/Scripts/Kinematics/InverseKinematic.cs
@@ -8,6 +8,7 @@ public class InverseKinematic : MonoBehaviour
     public int count = 5;
     [Range(0.1f, 3f)] public float size = 1;
     [Range(0.1f, 3f)] public float length = 1;
+    public Transform anchor;
     public Transform target;
 
     List<InverseKinematicSegment> segments = new List<InverseKinematicSegment>();
@@ -35,6 +36,16 @@ public class InverseKinematic : MonoBehaviour
 
             Vector2 position = (segment.parent != null) ? segment.parent.start : (Vector2)target.position;
             segment.Follow(position);
+        }
+        if (anchor)
+        {
+            int base_index = segments.Count - 1;
+            segments[base_index].start = anchor.position;
+
+            for (int i = base_index - 1; i >= 0; i--)
+            {
+                segments[i].start = segments[i + 1].end;
+            }
         }
     }
 }
